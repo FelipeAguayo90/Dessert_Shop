d0751841b588b6cbb8fedf85425fed5ca12637f0
Felipe Aguayo <felipeaguayo212@icloud.com>
2024-10-16 10:24:13 -0600

    added test cases for calculate cost and tax in test_icecream.py

--- test_icecream.py
+++ test_icecream.py
@@ -4,24 +4,30 @@ import dessert as d
 
 class TestIceCream(unittest.TestCase):
     def setUp(self):
-        self.i = d.IceCream("Cookies N Cream", 3, 1.50)
+        self.i = d.IceCream("Pistachio", 2, 0.79)
 
     def test_init(self):
-        self.assertEqual(self.i.name, "Cookies N Cream")
-        self.assertEqual(self.i.scoop_count, 3)
-        self.assertEqual(self.i.price_per_scoop, 1.50)
+        self.assertEqual(self.i.name, "Pistachio")
+        self.assertEqual(self.i.scoop_count, 2)
+        self.assertEqual(self.i.price_per_scoop, 0.79)
 
     def test_set_name(self):
         self.i.name = "Chocolate Mint"
         self.assertEqual(self.i.name, "Chocolate Mint")
 
     def test_set_scoop_count(self):
-        self.i.scoop_count = 2
-        self.assertEqual(self.i.scoop_count, 2)
+        self.i.scoop_count = 3
+        self.assertEqual(self.i.scoop_count, 3)
 
     def test_set_price_per_scoop(self):
         self.i.price_per_scoop = 1.25
         self.assertEqual(self.i.price_per_scoop, 1.25)
+
+    def test_calculate_cost(self):
+        self.assertEqual(self.i.calculate_cost(), 1.58)
+
+    def test_calculate_tax(self):
+        self.assertEqual(self.i.calculate_tax(), 0.11)
 
     def tearDown(self):
         return super().tearDown()
