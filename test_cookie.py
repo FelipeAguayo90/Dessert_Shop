a601266beb09254eb1ccb1a3998be3e9afffdd0d
Felipe Aguayo <felipeaguayo212@icloud.com>
2024-10-16 10:19:42 -0600

    added test cases for calculate cost and tax on test_cookie.py

--- test_cookie.py
+++ test_cookie.py
@@ -4,12 +4,12 @@ import dessert as d
 
 class TestCookie(unittest.TestCase):
     def setUp(self):
-        self.c = d.Cookie("Chocolate Chip", 12, 6.50)
+        self.c = d.Cookie("Chocolate Chip", 6, 3.99)
 
     def test_init(self):
         self.assertEqual(self.c.name, "Chocolate Chip")
-        self.assertEqual(self.c.cookie_quantity, 12)
-        self.assertEqual(self.c.price_per_dozen, 6.50)
+        self.assertEqual(self.c.cookie_quantity, 6)
+        self.assertEqual(self.c.price_per_dozen, 3.99)
 
     def test_set_name(self):
         self.c.name = "Sugar Cookie"
@@ -22,6 +22,12 @@ class TestCookie(unittest.TestCase):
     def test_set_price_per_dozen(self):
         self.c.price_per_dozen = 7.50
         self.assertEqual(self.c.price_per_dozen, 7.50)
+
+    def test_calculate_cost(self):
+        self.assertEqual(self.c.calculate_cost(), 2.00)
+
+    def test_calculate_tax(self):
+        self.assertEqual(self.c.calculate_tax(), 0.14)
 
     def tearDown(self):
         return super().tearDown()
